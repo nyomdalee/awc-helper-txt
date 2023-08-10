94d9461b338925f4c65f5eb7584102e6c0812a2e
Nyomdalee <139174287+Nyomdalee@users.noreply.github.com>
2023-08-10 17:20:08 +0200

    add SimpleAnime model for Tababular

---
+++ src/Models/SimpleAnime.cs
@@ -0,0 +1,22 @@
+﻿namespace AwcHelper.Txt.Models;
+internal class SimpleAnime
+{
+    public string Title { get; }
+    public string Score { get; }
+    public string Year { get; }
+    public string Episodes { get; }
+    public string EpDuration { get; }
+    public string Genres { get; }
+    public string Link { get; }
+
+    public SimpleAnime(Anime anime)
+    {
+        Title = anime.Title!;
+        Score = anime.Mean.ToString() ?? "none";
+        Year = anime.DeserializedStartDate.Year.ToString() ?? "unknown";
+        Episodes = anime.NumEpisodes.ToString() ?? "unknown";
+        EpDuration = anime.EpisodeDurationToString();
+        Genres = anime.GenresToString() ?? "none";
+        Link = $@"https://myanimelist.net/anime/{anime.Id}";
+    }
+}
